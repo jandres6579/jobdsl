0aba5d65e3a3fab51614250ab883310a91f719ac
Juan Andrés <jasanchez@odins.es>
2018-08-20 10:28:06 +0200

    Prueba añadir un segundo .groovy

---
+++ jobs/test.groovy
@@ -0,0 +1,25 @@
+project_name = "jenkins-pipeline-python-test" 
+repo = "https://github.com/jandres6579/prueba-test-1.git"
+repo_name = "repo" 
+
+
+pipelineJob(project_name) {
+    definition {
+        triggers{
+            scm('H/1 * * * *')
+        }
+
+        cpsScm{
+            scm {
+                git {
+                    remote {
+                        name(repo_name)
+                        url(repo)
+                    }
+                    scriptPath("Jenkinsfile")
+
+                }
+            }
+        }
+    }
+}
