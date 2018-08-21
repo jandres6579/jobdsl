2be1e3ec613764628aff32747038bdc7087b7ad5
Juan Andrés <jasanchez@odins.es>
2018-08-21 08:53:22 +0200

    Quitar basura

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -1,7 +1,18 @@
 project_name = "jenkins-pipeline-python" 
+/*
+Si quisiesemos incluir el job en una carpeta. Cambiaríamos la línea anteior por esta...
+project_name = "Proyecto-a/jenkins-pipeline-python-test" 
+*/
 repo = "https://github.com/jandres6579/prueba-test-1.git"
 repo_name = "repo" 
 
+/*
+Si quisiesemos incluir el job en una carpeta. Añadiríamos esto...
+folder('Proyecto-a') {
+    displayName('Proyect A')
+    description('Folder for proyect A')
+}
+*/
 
 pipelineJob(project_name) {
     definition {
